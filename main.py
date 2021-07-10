3de7fe138cf25b567f5bcb7a9f6bb85b807710a6
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-07-10 16:18:53 +0300

    v1.3.0
    Add support for rolling releases
    Add command-line argument stuff (run with --help or -?)

--- main.py
+++ main.py
@@ -4,15 +4,30 @@ import os
 import tempfile
 import zipfile
 from datetime import datetime
-from sys import exit
-from typing import Optional
+from enum import Enum
+from sys import exit, argv
+from typing import Optional, AnyStr
 
 import requests
 from tqdm import tqdm
 
+version = '1.3.0'
+
 local_meta_name = 'meta.json'
 repo_path = 'whitelilydragon/ShangMuArchitect'
-latest_release_url = 'https://api.github.com/repos/{}/releases/latest'.format(repo_path)
+repo_path_rolling = 'SMALauncher/SMARolling'
+latest_release_url_template = 'https://api.github.com/repos/{}/releases/latest'
+
+if ('--help' in argv) or ('-?' in argv):
+    print('SMALauncher v{}'.format(version))
+    print('A simple launcher/updater for Shang Mu Architect. Updates your game while keeping your settings intact!')
+    print()
+    print('Usage:')
+    print('\t{} [--help/-?] [--rolling/-R]'.format(argv[0]))
+    print()
+    print('\t--help/-?\t- displays this help message and exists')
+    print('\t--rolling/-R\t- enables updating to rolling versions of SMA')
+    exit(0)
 
 
 def load_json(name: str) -> Optional[dict]:
@@ -27,12 +42,28 @@ def load_json(name: str) -> Optional[dict]:
     return obj
 
 
-def update(local_meta: Optional[dict]) -> tuple[bool, Optional[dict]]:
+class UpdateResult(Enum):
+    FAIL = -1,
+    UP_TO_DATE = 0,
+    SUCCESS = 1
+
+
+def content_type_is_zip(content_type: AnyStr) -> bool:
+    return (content_type == 'application/zip') or (content_type == 'application/x-zip-compressed')
+
+
+def update(rolling: bool, local_meta: Optional[dict]) -> tuple[UpdateResult, Optional[dict]]:
+    if rolling:
+        print('Checking for rolling releases...')
+        latest_release_url = latest_release_url_template.format(repo_path_rolling)
+    else:
+        print('Checking for stable releases...')
+        latest_release_url = latest_release_url_template.format(repo_path)
     print('Fetching latest release from "{}"'.format(latest_release_url))
     res = requests.get(latest_release_url, headers={'Accept': 'application/vnd.github.v3+json'})
     if res.status_code != 200:
         print('Failed to fetch latest release! Got status code {}'.format(res.status_code))
-        return False, local_meta
+        return UpdateResult.FAIL, local_meta
     latest_json = res.json()
     latest_id = int(latest_json['id'])
     latest_name = str(latest_json['tag_name'])
@@ -45,7 +76,7 @@ def update(local_meta: Optional[dict]) -> tuple[bool, Optional[dict]]:
             print('We\'re out of date! Our version is {}, while latest is {}'.format(current_name, latest_name))
         else:
             print('We\'re up to date, yay!')
-            return True, local_meta
+            return UpdateResult.UP_TO_DATE, local_meta
     print('Downloading latest version {}'.format(latest_name))
     # find meta.json first
     latest_meta_dl = None
@@ -56,25 +87,25 @@ def update(local_meta: Optional[dict]) -> tuple[bool, Optional[dict]]:
     if latest_meta_dl is None:
         print('Latest release "{}" is missing a "meta.json" asset! Please contact Libbie.'
               .format(latest_name))
-        return False, local_meta
+        return UpdateResult.FAIL, local_meta
     res = requests.get(latest_meta_dl)
     if res.status_code != 200:
         print('Failed to fetch latest release metadata from "{}"! Got status code {}'
               .format(latest_meta_dl, res.status_code))
-        return False, local_meta
+        return UpdateResult.FAIL, local_meta
     latest_meta = res.json()
     zip_md5 = str(latest_meta['asset_md5']).lower()
     # download the game ZIP
     zip_dl = None
     for asset in latest_json['assets']:
         if str(asset['name']).startswith('Shang_Mu_Architect_') \
-                and str(asset['content_type']) == 'application/x-zip-compressed':
+                and content_type_is_zip(str(asset['content_type'])):
             zip_dl = str(asset['browser_download_url'])
             break
     if zip_dl is None:
-        print('Latest release "{}" is missing the game ZIP file (name starts with "Shang Mu Architect")!'
+        print('Latest release "{}" is missing the game ZIP file (name starts with "Shang_Mu_Architect_")!'
               'Please contact Libbie.'.format(latest_name))
-        return False, local_meta
+        return UpdateResult.FAIL, local_meta
     print('Downloading latest game ZIP from "{}"'.format(zip_dl))
     zip_name = zip_dl.split('/')[-1]
     tmp_name = tempfile.mktemp()
@@ -93,7 +124,7 @@ def update(local_meta: Optional[dict]) -> tuple[bool, Optional[dict]]:
     except KeyboardInterrupt:
         print('Interrupted by user, aborting')
         os.remove(tmp_name)
-        return False, local_meta
+        return UpdateResult.FAIL, local_meta
     print('Done downloading ZIP!')
     # verify MD5
     print('Verifying MD5 of downloaded ZIP...')
@@ -105,12 +136,12 @@ def update(local_meta: Optional[dict]) -> tuple[bool, Optional[dict]]:
     except KeyboardInterrupt:
         print('Interrupted by user, aborting')
         os.remove(tmp_name)
-        return False, local_meta
+        return UpdateResult.FAIL, local_meta
     zip_md5_actual = hash_md5.hexdigest().lower()
     if zip_md5_actual != zip_md5:
         print('Downloaded ZIP has incorrect MD5 hash! Should be {}, but is {}'.format(zip_md5, zip_md5_actual))
         os.remove(tmp_name)
-        return False, local_meta
+        return UpdateResult.FAIL, local_meta
     print('ZIP verified! Download was successful!')
     # back up gamedata.dat (settings, best times)
     if os.path.isdir('game'):
@@ -136,16 +167,16 @@ def update(local_meta: Optional[dict]) -> tuple[bool, Optional[dict]]:
     local_meta['release_id'] = latest_id
     local_meta['release_name'] = latest_name
     local_meta['exe_name'] = latest_meta['exe_name']
-    return True, local_meta
+    return UpdateResult.SUCCESS, local_meta
 
 
-def try_update(local_meta: Optional[dict]) -> tuple[bool, Optional[dict]]:
+def try_update(rolling: bool, local_meta: Optional[dict]) -> tuple[UpdateResult, Optional[dict]]:
     try:
-        return update(local_meta)
+        return update(rolling, local_meta)
     except Exception as err:
         print('Failed to update game due to exception:\n'
               '  {}: {}'.format(type(err).__name__, str(err)))
-        return False, local_meta
+        return UpdateResult.FAIL, local_meta
 
 
 def main():
@@ -163,7 +194,10 @@ def main():
                       '  {}: {}'.format(type(err).__name__, str(err)))
                 exit(1)
                 return
-    success, local_meta = try_update(local_meta)
+    rolling = ('--rolling' in argv) or ('-R' in argv)
+    result, local_meta = try_update(rolling, local_meta)
+    if result == UpdateResult.UP_TO_DATE and rolling:
+        result, local_meta = try_update(False, local_meta)
     if local_meta is None:
         print('First run failed! Please check your Internet connection.')
         input('Press Enter to exit... ')
@@ -171,7 +205,7 @@ def main():
         return
     with open(local_meta_name, 'wt') as fp:
         json.dump(local_meta, fp)
-    if not success:
+    if result == UpdateResult.FAIL:
         print('Failed to update game.')
         answer = input('Launch anyways? [Y/n] ').lower().strip()[:1]
         if answer == 'n':
@@ -181,7 +215,7 @@ def main():
     print('Running the game!')
     os.chdir('game')
     os.spawnl(os.P_NOWAIT, exe_name, exe_name)
-    if not success:
+    if result == UpdateResult.FAIL:
         input('Press Enter to exit...')
         exit(1)
 
